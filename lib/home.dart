0d0de9e620ee4c0fbf23efeb49985e3dc6827a7b
Niraj Kumar <nirajs9kr@gmail.com>
2023-07-13 23:40:58 +0530

    app is done , next->work on ui

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:torch_controller/torch_controller.dart';
 // import 'package:flutter_flashlight/flutter_flashlight.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var isActive = false;
+  var controller= TorchController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,15 +26,40 @@ class _HomePageState extends State<HomePage> {
           Expanded(
               child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Image.asset(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
                     isActive ? 'assets/torch_on.jpg' : 'assets/torch_off.jpg',
-                    width: 50,
-                    height: 50,
+                    width: 200,
+                    height: 200,
+                    color: isActive
+                        ? null
+                        : Color.fromARGB(255, 255, 255, 255).withOpacity(0.1),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CircleAvatar(
+                    minRadius: 30,
+                    maxRadius: 45,
+                    child: Transform.scale(
+                      scale: 1.5,
+                      child: IconButton(
+                        onPressed: () {
+                          controller.toggle();
+                          isActive=!isActive;
+                          setState(() {
+                            
+                          });
+                        },
+                        icon: const Icon(Icons.power_settings_new),
+                      ),
                     ),
-                    
-              ],
+                  ),
+                ],
+              ),
             ),
           )),
           const Text(
